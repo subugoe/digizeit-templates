909e7d395a2cdd619d56e5771120e6d8a1583558
Jochen Kothe <kothe@sub.uni-goettingen.de>
2013-08-26 16:31:11 +0200

    add getnoocr.php

---
+++ scripts/getnoocr.php
@@ -0,0 +1,23 @@
+<?php
+$strOCRPath = '/srv/www/chroot/digizeit/digizeit/htdocs/gdzocr/';
+$strDZPath = '/storage_lokal/indexed_mets/';
+
+#$strOCRPath = '/hidrive/sftp/users/digizeit/gdzocr/';
+#$strDZPath = '/hidrive/sftp/users/digizeit/mets_repository/indexed_mets/';
+
+$arrOCR = scandir($strOCRPath);
+$arrDZ = scandir($strDZPath);
+
+foreach($arrDZ as $k=>$v) {
+    $ppn = trim(str_replace('.xml','',trim($v)));
+    if(substr($ppn,0,3)!='PPN' || strlen($ppn)<=12) {
+        unset($arr[$k]);
+    } else {
+        $arrDZ[$k] = $ppn;
+    } 
+}
+
+$arr = array_diff($arrDZ,$arrOCR);
+
+echo(json_encode($arr));
+?>
