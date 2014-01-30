c2914ad1f2e247d4118a40a3e3bf4b9e40e8c8a5
Jochen Kothe <kothe@sub.uni-goettingen.de>
2014-01-30 20:41:01 +0100

    build arrResult

--- scripts/pagecount.php
+++ scripts/pagecount.php
@@ -707,13 +707,13 @@ print_r('</pre>');
 
         $arrSolr = unserialize(file_get_contents($this->config['solrPhpsUrl'] . $strSolr));
         foreach($arrSolr['response']['docs'] as $key=>$val) {
-print_r($val);
-
             foreach($val as $k=>$v) {
-print_r($k."\n");
-print_r($v);
+print_r($this->config['arrSerFields']);
+print_r('<br />');
                 if(in_array($k, $this->config['arrSerFields'])) {
                     $arrSolr['response']['docs'][$key][$k] = $this->_unserialize($v);
+print_r('[response][docs]['.$key.']['.$k.']');                    
+print_r('<br />');
                 }
             }
         }
