f7ba955c089d189c69c560a385aaa97120b357ad
Jochen Kothe <kothe@sub.uni-goettingen.de>
2014-02-08 21:56:14 +0100

    add oai2 to repo

--- scripts/oai2/index.php
+++ scripts/oai2/index.php
@@ -49,9 +49,10 @@ print_r($xml->oai->saveXML());
 $time = time() - $xml->conf['MAIN']['expirationDate'];
 $time = time();
 $d = dir($xml->conf['MAIN']['tmpDir']);
+file_put_contents($xml->conf['MAIN']['tmpDir'].'bla.log',$xml->conf['MAIN']['tmpDir']."\n",FILE_APPEND);
 while (false !== ($entry = $d->read())) {
     if (is_file($oaiTokenDir . $entry) && substr($entry,0,3) == 'oai') {
-file_put_contents('/srv/www/chroot/digizeit/digizeit/tmp/bla.log',$entry."n",FILE_APPEND);
+file_put_contents($xml->conf['MAIN']['tmpDir'].'bla.log',$entry."\n",FILE_APPEND);
         if (filemtime($oaiTokenDir . $entry) < $time) {
             unlink($oaiTokenDir . $entry);
         }
