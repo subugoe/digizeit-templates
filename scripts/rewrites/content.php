af06cec2466185e2799a33c62ca3f810ef1148f9
Jochen Kothe <kothe@sub.uni-goettingen.de>
2014-04-01 11:16:12 +0200

    add response header to content.php

--- scripts/rewrites/content.php
+++ scripts/rewrites/content.php
@@ -86,6 +86,7 @@ if (count($arrTmp) != 4) {
     if (!$acl) {
         $arrInfo = getimagesize($restrictImg);
         $img = file_get_contents($restrictImg);
+        header('HTTP/1.0 401 Unauthorized');
         header('Content-type: ' . $arrInfo['mime']);
         echo $img;
         exit();
