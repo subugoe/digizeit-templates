b7dfe02aa38970be8160d5a58148189c692226dc
Jochen Kothe <kothe@sub.uni-goettingen.de>
2014-09-24 13:29:24 +0200

    ACL for fulltextview

--- scripts/rewrites/content.php
+++ scripts/rewrites/content.php
@@ -25,7 +25,7 @@
  * ************************************************************* */
 define('__DZROOT__', realpath(__DIR__ . '/../../../../'));
 
-file_put_contents(__DZROOT__.'/tmp/bla.log', json_encode($_COOKIE)."\n", FILE_APPEND);
+//file_put_contents(__DZROOT__.'/tmp/bla.log', json_encode($_COOKIE)."\n", FILE_APPEND);
 //file_put_contents(__DZROOT__.'/tmp/bla1.log', json_encode($GLOBALS)."\n", FILE_APPEND);
 
 error_reporting(0);
