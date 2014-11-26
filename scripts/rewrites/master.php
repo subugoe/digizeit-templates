4ff9fc5c7e22a1151742eb2cd31870977ceec4a4
Jochen Kothe <kothe@sub.uni-goettingen.de>
2014-11-26 14:53:10 +0100

    URL Switch

--- scripts/rewrites/master.php
+++ scripts/rewrites/master.php
@@ -63,11 +63,11 @@ if(strpos($arrTest[0],'200')!==false) {
 }
         
 //debug
-file_put_contents(__DZROOT__.'/tmp/debug.log',$URL."\n",FILE_APPEND);
-file_put_contents(__DZROOT__.'/tmp/debug.log',$URL1."\n",FILE_APPEND);
+//file_put_contents(__DZROOT__.'/tmp/debug.log',$URL."\n",FILE_APPEND);
+//file_put_contents(__DZROOT__.'/tmp/debug.log',$URL1."\n",FILE_APPEND);
 // Stupid but without that brake ContentServer an OpenVZ are overfloated
 usleep(30);
 
-header('location: ' . $URL);
+header('location: ' . $URL2);
 exit();
 ?>
